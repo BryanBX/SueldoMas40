edf8a7f5783a809f77a0a83b501cfb0896072519
BryanBX <bryangp93@gmail.com>
2019-10-01 16:07:40 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salario_mas_40_porciento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sueldo, aumento;
+            Console.WriteLine("Escriba sueldo");
+            sueldo = int.Parse(Console.ReadLine());
+            aumento = (sueldo * 0.4) + sueldo;
+            Console.WriteLine("Su nuevo sueldo es: {0}", aumento);
+            Console.ReadKey();
+
+        }
+    }
+}
